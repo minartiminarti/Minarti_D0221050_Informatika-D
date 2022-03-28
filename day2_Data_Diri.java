9d04f9c566d2efbf20f295c4b81c9a43893030af
minartiminarti <99627009+minartiminarti@users.noreply.github.com>
2022-03-28 22:14:51 +0800

    day2_Data_Diri.java

---
+++ day2_Data_Diri.java
@@ -0,0 +1,11 @@
+package tugas;
+
+public class day2DataDiri {
+
+  public static void main(String[] args) {
+    System.out.println("selvia herawati");
+    System.out.println("Informatika C");
+    System.out.println("D0221032");
+    
+  }
+}
